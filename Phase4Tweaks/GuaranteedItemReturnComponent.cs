c9a91f9881da1b8662d0af060285be91b1a49870
Moffein <22742925+Moffein@users.noreply.github.com>
2024-02-15 16:45:48 -0800

    Add project files.

---
+++ Phase4Tweaks/GuaranteedItemReturnComponent.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.Networking;
+
+namespace Phase4Tweaks
+{
+    public class GuaranteedItemReturnComponent : MonoBehaviour
+    {
+        public static float cooldownDuration = 30f;
+        public static float closeRangeDistanceSqr = 30f * 30f;
+        public static float closeRangeCooldownMult = 3f;
+        private List<AttackerCooldown> cooldowns = new List<AttackerCooldown>();
+
+        private void FixedUpdate()
+        {
+            if (!NetworkServer.active) return;
+            float closeRangeTime = Time.fixedDeltaTime * closeRangeCooldownMult;
+            foreach(AttackerCooldown ac in cooldowns)
+            {
+                bool close = false;
+                if (ac.attacker)
+                {
+                    float distSqr = (ac.attacker.transform.position - base.transform.position).sqrMagnitude;
+                    if (distSqr <= GuaranteedItemReturnComponent.closeRangeDistanceSqr) close = true;
+                }
+                ac.ReduceCooldown(close ? closeRangeTime : Time.fixedDeltaTime);
+            }
+
+            //Remove dead attackers
+            cooldowns = cooldowns.Where(ac => ac.attacker != null).ToList();
+        }
+
+        public void SetStealItemCooldown(GameObject attacker)
+        {
+            if (!attacker) return;
+            AttackerCooldown ac = cooldowns.Find(c => c.attacker == attacker);
+            if (ac != null)
+            {
+                ac.SetCooldown(GuaranteedItemReturnComponent.cooldownDuration);
+            }
+            else
+            {
+                ac = new AttackerCooldown(attacker, GuaranteedItemReturnComponent.cooldownDuration);
+                cooldowns.Add(ac);
+            }
+        }
+
+        public bool ShouldGuaranteedSteal(GameObject attacker)
+        {
+            if (!attacker) return false;
+            AttackerCooldown ac = cooldowns.Find(c => c.attacker == attacker);
+            if (ac == null)
+            {
+                ac = new AttackerCooldown(attacker, 0f);
+                cooldowns.Add(ac);
+            }
+
+            return ac.CanSteal();
+        }
+
+        private class AttackerCooldown
+        {
+            public GameObject attacker;
+            float cooldown;
+
+            public AttackerCooldown(GameObject attacker, float cooldown)
+            {
+                this.attacker = attacker;
+                this.cooldown = cooldown;
+            }
+
+            public void SetCooldown(float duration)
+            {
+                cooldown = duration;
+            }
+            
+            public void ReduceCooldown(float duration)
+            {
+                if (cooldown > 0f)
+                {
+                    cooldown -= duration;
+                }
+            }
+
+            public bool CanSteal()
+            {
+                return cooldown <= 0f;
+            }
+        }
+    }
+}
---
+++ Phase4Tweaks/Phase4TweaksPlugin.cs
@@ -0,0 +1,65 @@
+﻿using BepInEx;
+using System;
+using UnityEngine.Networking;
+using RoR2;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+using System.Security.Cryptography;
+
+namespace R2API.Utils
+{
+    [AttributeUsage(AttributeTargets.Assembly)]
+    public class ManualNetworkRegistrationAttribute : Attribute
+    {
+    }
+}
+
+namespace Phase4Tweaks
+{
+    [BepInPlugin("com.Moffein.Phase4Tweaks", "Phase4Tweaks", "1.0.0")]
+    public class Phase4TweaksPlugin : BaseUnityPlugin
+    {
+        private static BodyIndex brotherHurtIndex;
+
+        private void Awake()
+        {
+            RoR2Application.onLoad += OnLoad;
+            GameObject brotherHurtPrefab = Addressables.LoadAssetAsync<GameObject>("RoR2/Base/Brother/BrotherHurtBody.prefab").WaitForCompletion();
+            brotherHurtPrefab.AddComponent<GuaranteedItemReturnComponent>();
+
+            On.RoR2.HealthComponent.TakeDamage += HealthComponent_TakeDamage;
+        }
+
+        private void HealthComponent_TakeDamage(On.RoR2.HealthComponent.orig_TakeDamage orig, HealthComponent self, DamageInfo damageInfo)
+        {
+            orig(self, damageInfo);
+
+            if (!damageInfo.rejected && self.body.bodyIndex == brotherHurtIndex && damageInfo.attacker)
+            {
+                GuaranteedItemReturnComponent g = self.GetComponent<GuaranteedItemReturnComponent>();
+                if (g && g.ShouldGuaranteedSteal(damageInfo.attacker))
+                {
+                    CharacterBody attackerBody = damageInfo.attacker.GetComponent<CharacterBody>();
+                    if (attackerBody && attackerBody.inventory)
+                    {
+                        ReturnStolenItemsOnGettingHit rsi = self.GetComponent<ReturnStolenItemsOnGettingHit>();
+                        if (rsi && rsi.itemStealController)
+                        {
+                            bool returned = rsi.itemStealController.ReclaimItemForInventory(attackerBody.inventory);
+                            if (returned)
+                            {
+                                Debug.Log("Phase4Tweaks: Guaranteed item return.");
+                                g.SetStealItemCooldown(damageInfo.attacker);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        private void OnLoad()
+        {
+            brotherHurtIndex = BodyCatalog.FindBodyIndex("BrotherHurtBody");
+        }
+    }
+}
